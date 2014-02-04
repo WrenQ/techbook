ce763a50b1a116829c8cff4b2ea278714ec89466
José Alcántara Muñoz <jose.alcantaramunoz@alum.uca.es>
2014-02-04 18:52:08 +0100

    Primera adaptación a postgres

--- models/usuarios.js
+++ models/usuarios.js
@@ -1,15 +1,9 @@
 //llamada al paquete mysql
-var mysql = require('mysql'),
-//creamos la conexion a nuestra base de datos con los datos de acceso de cada uno
-connection = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root', 
-        port: '3307', 
-        password: '',
-        database: 'techbook'
-    }
-);
+var pg = require('pg'),
+    conString = "postgres://iowvvikynknmfu:waBSsvgpeZ45Z2rdyrfJDZxClV@ec2-23-21-185-168.compute-1.amazonaws.com:5432/dpoake3pqalcl",
+    client = new pg.Client(conString);
+//Nos conectamos a la Base de Datos
+client.connect();
  
 //creamos un objeto para ir almacenando todo lo que necesitemos
 var usuarioModel = {};
@@ -17,32 +11,29 @@ var usuarioModel = {};
 //obtenemos un articulo por su tipo
 usuarioModel.getPass = function(nombre, pass, callback)
 {
-    if (connection)
-    {
-        connection.query('SELECT * FROM usuario WHERE usr_login = ? AND usr_pass = ? ', [nombre, pass], function(error, row) {
-            if(error)
-            {
-                throw error;
-            }
-            else
-            {   
-                callback(null, row);
-            }
-        });
-    }
+    client.query('SELECT * FROM usuario WHERE usr_login = ? AND usr_pass = ? ', [nombre, pass], function(error, row) {
+        if(error)
+        {
+            throw error;
+        }
+        else
+        {   
+            callback(null, row);
+        }
+    });
 }
 
 usuarioModel.setUser = function(login, pass, nombre, apell, mail, direccion, callback)
 {
-    if(connection){
-        connection.query('INSERT INTO usuario VALUES(?,?,?,?,?,?)', [login, pass, nombre, apell, mail, direccion], function(error, row) {
-            if(error){
-                throw error;
-            } else {
-                callback(null, row);
-            }
-        });
-    }
+    
+    client.query('INSERT INTO usuario VALUES(?,?,?,?,?,?)', [login, pass, nombre, apell, mail, direccion], function(error, row) {
+        if(error){
+            throw error;
+        } else {
+            callback(null, row);
+        }
+    });
+    
 }
  
 //exportamos el objeto para tenerlo disponible en la zona de rutas
