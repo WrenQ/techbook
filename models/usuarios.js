2dd4870e57e614d3a39d621f0ef443bb001729d6
Francisco Madueño <pako_xp2000@hotmail.com>
2014-02-05 19:49:50 +0100

    index y usuarios js con jslint y jshint

--- models/usuarios.js
+++ models/usuarios.js
@@ -4,42 +4,35 @@ var pg = require('pg'),
     client = new pg.Client(conString);
 //Nos conectamos a la Base de Datos
 client.connect();
-client.query('set schema \'techbook\'', function(err, rows) {
-    if(err){
+client.query('set schema \'techbook\'', function (err) {
+    if (err) {
         console.log(err);
     }
 });
 //creamos un objeto para ir almacenando todo lo que necesitemos
 var usuarioModel = {};
- 
+
 //obtenemos un articulo por su tipo
-usuarioModel.getPass = function(nombre, pass, callback)
-{
-    client.query('SELECT * FROM usuario WHERE usr_login = $1 AND usr_pass = $2 ', [nombre, pass], function(error, results) {
-        if(error)
-        {
+usuarioModel.getPass = function (nombre, pass, callback) {
+    client.query('SELECT * FROM usuario WHERE usr_login = $1 AND usr_pass = $2 ', [nombre, pass], function (error, results) {
+        if (error) {
             console.log(error);
-        }
-        else
-        {   
+        } else {
             console.log(results.rows);
             callback(null, results.rows);
         }
     });
-}
+};
 
-usuarioModel.setUser = function(login, pass, nombre, apell, mail, direccion, callback)
-{
-    
-    client.query('INSERT INTO usuario VALUES($1,$2,$3,$4,$5,$6)', [login, pass, nombre, apell, mail, direccion], function(error, results) {
-        if(error){
+usuarioModel.setUser = function (login, pass, nombre, apell, mail, direccion, callback) {
+    client.query('INSERT INTO usuario VALUES($1,$2,$3,$4,$5,$6)', [login, pass, nombre, apell, mail, direccion], function (error, results) {
+        if (error) {
             console.log(error);
         } else {
             callback(null, results.rows);
         }
     });
-    
-}
- 
+};
+
 //exportamos el objeto para tenerlo disponible en la zona de rutas
 module.exports = usuarioModel;
