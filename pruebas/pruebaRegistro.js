dfaa781a43d5200d49a88cc84fbab2a75e4a7189
Francisco Madueño <pako_xp2000@hotmail.com>
2014-02-06 14:38:01 +0100

    sin ¿?

--- pruebas/pruebaRegistro.js
+++ pruebas/pruebaRegistro.js
@@ -1,10 +1,8 @@
 var Browser = require("zombie");
 var assert = require("assert");
 
-browser = new Browser();
-browser.visit("http://localhost:3000/", function () {
-    browser.fill("Usuario", "EArauz").fill("Contraseña", "12345").pressButton("Entrar", function () {
-        assert.ok(browser.success);
-        assert.equal(browser.text("title"), "Bienvenido");
-    });
+browser = new Browser({ debug: true });
+
+browser.visit("http://techbook.herokuapp.com/", function () {
+    browser.clickLink("Windows Phone", function () {});
 });
