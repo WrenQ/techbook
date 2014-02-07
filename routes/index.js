6b5bd2bb74660bb78b947e39c9074ac2c4676f4f
José Alcántara Muñoz <jose.alcantaramunoz@alum.uca.es>
2014-02-07 16:21:54 +0100

    Inicio de sesión sin nombre de usuario

--- routes/index.js
+++ routes/index.js
@@ -87,19 +87,19 @@ module.exports = function (app) {
     });
 
     app.post('/', function (req, res) {
-        if(req.body.nUsuario.length !== 0 && req.body.clave.length !== 0) {
-            usuarioModel.getPass(req.body.nUsuario, req.body.clave, function (err, results) {
-                usuario = results[0].usr_nombre;
-                usuarioLogin = results[0].usr_login;
+        //if(req.body.nUsuario.length !== 0 && req.body.clave.length !== 0) {
+        //    usuarioModel.getPass(req.body.nUsuario, req.body.clave, function (err, results) {
                 if (results.length !== 0) {
+                    usuario = results[0].usr_nombre;
+                    usuarioLogin = results[0].usr_login;
                     res.render('index', { usuario : usuario });
                 } else {
                     res.render('login', { texto: "Usuario o contraseña incorrectos.", title: "Inicio sesión", usuarios: usuarioModel });
                 }
             });
-        } else {
-            res.render('login', { texto: "No has indicado el nombre de usuario o la contraseña.", title: "Inicio sesión", usuarios: usuarioModel });
-        }
+        //} else {
+        //    res.render('login', { texto: "No has indicado el nombre de usuario o la contraseña.", title: "Inicio sesión", usuarios: usuarioModel });
+        //}
     });
 
     app.post('/procesarRegistro', function (req, res) {
